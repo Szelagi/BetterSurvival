86a31c37eb8489d9d2646daea9642c669141b06d
Kamil Szelągiewicz <kamil.szelagiewicz@gmail.com>
2024-12-05 16:41:31 +0100

    plugin minecraft init

---
+++ src/main/java/pl/szelagi/betterSurvival/BetterSurvival.java
@@ -0,0 +1,17 @@
+package pl.szelagi.betterSurvival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterSurvival extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
